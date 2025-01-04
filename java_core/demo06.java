0d3e0bbcdf341f8ef36c9625ec124ca95aadb22f
Srujan Sai Manukonda <151755580+Srujansai19@users.noreply.github.com>
2025-01-04 12:20:40 +0530

    Add files via upload

---
+++ java_core/demo06.java
@@ -0,0 +1,13 @@
+
+class demo06 {
+    public static void main(String[] args) {
+
+        String Username = "Srujan";
+        String Password = "Srujan_123";
+
+        while((Username == "Srujan") && (Password == "Srujan_123")) {
+            System.out.println("Welcome to Mallareddy university");
+            break;
+        }
+        }
+ }
