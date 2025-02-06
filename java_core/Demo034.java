77fc865fc6992cf3a3cbb01c4e36bfeaebf0d74d
Srujan Sai Manukonda <151755580+Srujansai19@users.noreply.github.com>
2025-02-06 22:07:32 +0530

    Even or Odd number

--- java_core/Demo034.java
+++ java_core/Demo034.java
@@ -5,7 +5,6 @@ public class Demo034 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
-
         if (num % 2 == 0) {
             System.out.println(num + " is even.");
         } else {
