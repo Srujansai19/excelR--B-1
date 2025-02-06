92caddf446785c7c0458c8e7162e4d99dfb800d8
Srujan Sai Manukonda <151755580+Srujansai19@users.noreply.github.com>
2025-02-06 22:00:53 +0530

    Leap Year

--- java_core/Demo021.java
+++ java_core/Demo021.java
@@ -3,7 +3,7 @@ public class Demo021 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter the year: ");
-            
+        
             int year = sc.nextInt();
             if ((year % 4 == 0)&&(year % 100 != 0)||(year % 400 == 0)) {
                 System.out.println(year + " is a leap year");
