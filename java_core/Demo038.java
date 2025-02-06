a5853bc122771ffd2e2877ed577bc3a01b2dfc21
Srujan Sai Manukonda <151755580+Srujansai19@users.noreply.github.com>
2025-02-06 22:09:25 +0530

    Lowercase or Uppercase

--- java_core/Demo038.java
+++ java_core/Demo038.java
@@ -6,6 +6,7 @@ public class Demo038 {
         System.out.print("Enter a character: ");
         char ch = scanner.next().charAt(0);
 
+        
         if (Character.isUpperCase(ch)) {
             System.out.println(ch + " is an uppercase letter.");
         } else if (Character.isLowerCase(ch)) {
