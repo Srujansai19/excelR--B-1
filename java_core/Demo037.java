1c03e3fe222a94ab02382daebbc09d78fcf045ec
Srujan Sai Manukonda <151755580+Srujansai19@users.noreply.github.com>
2025-02-06 22:08:49 +0530

    Greatest of 4 numbers

--- java_core/Demo037.java
+++ java_core/Demo037.java
@@ -4,6 +4,7 @@ public class Demo037 {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter First number: ");
             int num1 = sc.nextInt();
+            
       
             System.out.println("Enter Second number: ");
             int num2 = sc.nextInt();
