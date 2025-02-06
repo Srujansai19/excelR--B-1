8d52b009be5f7e89fa067cf3e81798e15ab544a7
Srujan Sai Manukonda <151755580+Srujansai19@users.noreply.github.com>
2025-02-06 22:02:40 +0530

    Friend's Details

--- java_core/Demo025.java
+++ java_core/Demo025.java
@@ -5,6 +5,7 @@ public class Demo025 {
         long[] number = {70324443,24890112,987839991,776891011,1090775432};
 
         for(int i =0; i<f1.length; i++){
+            
             System.out.println("Friend "+(i+1)+" :"+f1[i]+address[i]+number[i]);
         }
         
