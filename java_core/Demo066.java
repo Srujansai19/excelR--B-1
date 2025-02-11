1ecb72237099dac3d8f4b2683e5c00699c8890f6
Srujan Sai Manukonda <151755580+Srujansai19@users.noreply.github.com>
2025-02-11 10:52:02 +0530

    index of array

--- java_core/Demo066.java
+++ java_core/Demo066.java
@@ -1,7 +1,7 @@
 public class Demo066 {
     public static void main(String[] args) {
         int[] arr = {10,20,30,40,50};
-
+//index of array
         for(int i=0;i<arr.length;i++){
             System.out.println("Index of "+arr[i] + ": "+i);
         }
