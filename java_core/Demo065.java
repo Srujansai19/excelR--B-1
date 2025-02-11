f3dedf6f25819e75efdf5d23edc3494b5317d94e
Srujan Sai Manukonda <151755580+Srujansai19@users.noreply.github.com>
2025-02-11 10:51:21 +0530

    elements of array

--- java_core/Demo065.java
+++ java_core/Demo065.java
@@ -1,7 +1,7 @@
 public class Demo065 {
     public static void main(String[] args) {
         int[] arr = {10,20,30,40,50};
-
+//elements of array
         System.out.println("Array elements:");
         for(int i: arr){
             System.out.print(i+" ");
